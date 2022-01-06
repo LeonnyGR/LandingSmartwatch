c1559159c7724b0627bf56f2245ebae937a44a3e
LeonnyGR <leonnyraiz@gmail.com>
2022-01-06 17:19:09 -0300

    Projeto-Watch

---
+++ script.js
@@ -0,0 +1,7 @@
+function trocarImagemB(){
+    document.querySelector(".img-princ").setAttribute('src', 'images/smart-white.png');
+}
+
+function trocarImagemP(){
+    document.querySelector(".img-princ").setAttribute('src', 'images/smart-black.png');
+}
